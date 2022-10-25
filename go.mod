9646e475e64f35a79b75bfd3c28c4e81ca885e47
dvrkv <dnsvrkv@yandex.ru>
2022-10-25 15:21:41 +0300

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dvrkv/Go-smtp-email-sender
+
+go 1.18
+
+require github.com/joho/godotenv v1.4.0
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/smtp"
+	"os"
+	"text/template"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func delay() {
+	time.Sleep(10 * time.Second)
+}
+
+func sendMail(subject string, templatePath string, to []string) {
+	//get html
+	var body bytes.Buffer
+	t, err := template.ParseFiles(templatePath)
+	t.Execute(&body, struct {
+		Name  string
+		Today time.Time
+	}{Name: "dear subscriber!", Today: time.Now()})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	godotenv.Load(".env")
+	auth := smtp.PlainAuth(
+		"",
+		os.Getenv("email"),
+		os.Getenv("password"),
+		"smtp.gmail.com",
+	)
+	headers := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";"
+	msg := "Subject: " + subject + "\n" + headers + "\n\n" + body.String()
+	delay()
+	err = smtp.SendMail(
+		"smtp.gmail.com:587",
+		auth,
+		os.Getenv("email"), //letter sender
+		to,                 //letter recipient
+		[]byte(msg),        //whole message
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	recipients := []string{"testdvrkv@gmail.com"}
+	sendMail(
+		"Some newsletter",
+		"./static/htmlBody.html",
+		recipients,
+	)
+}
